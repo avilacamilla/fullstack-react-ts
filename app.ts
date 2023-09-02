15424ff27fb2b30bb84c832401c13e9444af5964
Camilla Avila <avilacamilla@outlook.com>
2023-09-02 10:45:35 -0300

    feature: Add opcional properties in interfaces and objects

--- app.ts
+++ app.ts
@@ -2,9 +2,11 @@
 interface Person {
     name: string,
     age: number,
+    address?: string //'?' antes torna esse objeto opcional
 }
 
 const pessoa: Person = {
     name: 'Camilla',
     age: 29,
+    address: 'Rua das Flores - Girassol - Rio de Janeiro, RJ'
 }
